51fae971e214bd1520b7867290b26b2bc2c4dfa9
Daniel Dawidowski <dvds1987@gmail.com>
2020-01-06 11:24:03 +0100

    Change style

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
 document.querySelector("#date").valueAsDate = new Date();
 
 
+const header = document.querySelector(".row");
+const sectionOne = document.querySelector("section");
+
+const sectionOneOptions = {
+  rootMargin: "-200px 0px 0px 0px"
+};
+
+const sectionOneObserver = new IntersectionObserver(function(
+  entries,
+  sectionOneObserver
+) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      header.classList.add("nav-change");
+    } else {
+      header.classList.remove("nav-change");
+    }
+  });
+},
+sectionOneOptions);
+
+sectionOneObserver.observe(sectionOne);
+       
