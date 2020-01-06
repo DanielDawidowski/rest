32bc0717467ec4c1b3701c52505e034fa068ac1c
Daniel Dawidowski <dvds1987@gmail.com>
2020-01-06 17:31:35 +0100

    Add Modal

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 document.querySelector("#date").valueAsDate = new Date();
 
+////////////////// NAV /////////////////////
 
 const header = document.querySelector(".row");
 const sectionOne = document.querySelector("section");
@@ -23,4 +24,42 @@ const sectionOneObserver = new IntersectionObserver(function(
 sectionOneOptions);
 
 sectionOneObserver.observe(sectionOne);
-       
+
+////////////////// NAV /////////////////////
+
+//////////////// MODAL //////////////////
+
+const addModal = document.getElementById('add-modal');
+const startMenuButton = document.querySelector('.lunch__menu button')
+const backdrop = document.getElementById('backdrop');
+const closeMenuButton = addModal.querySelector('.close');
+
+const toggleBackdrop = () => {
+    backdrop.classList.toggle('visible');
+}
+
+const toggleMenuModal = () => {
+    addModal.classList.toggle('visible');
+    toggleBackdrop();
+} 
+
+const backdropClickHandler = () => {
+    toggleMenuModal();
+}
+
+const closeMenuModal = () => {
+    toggleMenuModal()
+}
+
+startMenuButton.addEventListener('click', toggleMenuModal);
+backdrop.addEventListener('click', backdropClickHandler);
+closeMenuButton.addEventListener('click', closeMenuModal);
+
+//////////////// MODAL //////////////////
+
+
+
+
+
+
+/////////////// MODAL //////////////////
