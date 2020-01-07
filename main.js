b4ee71f3943c98ced54414dc94900e90324f0112
Daniel Dawidowski <dvds1987@gmail.com>
2020-01-07 14:22:21 +0100

    Add Lightbox to Gallery

--- main.js
+++ main.js
@@ -30,7 +30,7 @@ sectionOneObserver.observe(sectionOne);
 //////////////// MODAL //////////////////
 
 const addModal = document.getElementById('add-modal');
-const startMenuButton = document.querySelector('.lunch__menu button')
+const startMenuButton = document.querySelector('.lunch__menu .menu');
 const backdrop = document.getElementById('backdrop');
 const closeMenuButton = addModal.querySelector('.close');
 
@@ -57,9 +57,65 @@ closeMenuButton.addEventListener('click', closeMenuModal);
 
 //////////////// MODAL //////////////////
 
+//////////////// GALLERY //////////////////
+
+const lightBoxContainer = document.querySelector('.lightbox-big');
+const lightboxImage = document.querySelector('.lightbox-img');
+const counter = document.querySelector('.lightbox-counter');
+const prevButton = document.querySelector('.prev');
+const nextButton = document.querySelector('.next');
+const lightboxText = document.querySelector('.lightbox-text');
+const lightboxItems = document.querySelector('.gallery-items').children;
+let index;
+let imgSrc;
+
+for (let i = 0; i<lightboxItems.length; i++){
+    lightboxItems[i].querySelector(".far").addEventListener('click',() => {
+        // console.log(lightboxItems[i].querySelector('h2'))
+        index=i;
+        changeImage();
+        lightbox();
+    })
+}
+
+lightBoxContainer.addEventListener("click", (event) => {
+    if(event.target !== lightboxImage && event.target !== prevButton && event.target !== nextButton) {
+        lightbox()
+    }
+})
+
+const next = () => {
+    if(index == lightboxItems.length - 1) {
+        index=0
+    } else {
+        index++;
+    }
+    changeImage();
+}
+
+const prev = () => {
+    if(index == 0) {
+        index = lightboxItems.length - 1;
+    } else {
+        index--;
+    }
+    changeImage()
+}
+
+const lightbox = () => {
+    lightBoxContainer.classList.toggle('open');
+}
+
+const changeImage = () => {
+    imgSrc = lightboxItems[index].querySelector('img').getAttribute('src');
+    lightboxImage.src = imgSrc;
+    counter.innerHTML = (index + 1) + " of " + lightboxItems.length;
+    lightboxText.innerHTML = lightboxItems[index].querySelector('h2').innerHTML;
+}
 
 
 
 
 
-/////////////// MODAL //////////////////
+
+//////////////// GALLERY //////////////////
